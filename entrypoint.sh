f74c1c5ce236c773b647d5cbea7e575cee1fd3eb
jordanckcm <neiljordan@ckcm.edu.ph>
2026-09-22 19:37:03 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Force only mpm_prefork to be active at runtime (not just build time)
+rm -f /etc/apache2/mods-enabled/mpm_event.load /etc/apache2/mods-enabled/mpm_event.conf
+rm -f /etc/apache2/mods-enabled/mpm_worker.load /etc/apache2/mods-enabled/mpm_worker.conf
+ln -sf /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
+ln -sf /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
+
+# Bind to Railway's assigned port
+sed -i "s/80/${PORT:-8080}/g" /etc/apache2/ports.conf /etc/apache2/sites-enabled/000-default.conf
+
+exec apache2-foreground
