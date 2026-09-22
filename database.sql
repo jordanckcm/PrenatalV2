29e40da57f59e54a1164f7e6927f1c2f4c27cfc9
jordanckcm <neiljordan@ckcm.edu.ph>
2026-09-22 18:11:41 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -12,6 +12,8 @@ USE `prenatal`;
 -- ----------------------------------------------------------------------------
 -- 1. Users Table
 -- ----------------------------------------------------------------------------
+SET FOREIGN_KEY_CHECKS = 0;
+
 CREATE TABLE IF NOT EXISTS `users` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(50) NOT NULL UNIQUE,
